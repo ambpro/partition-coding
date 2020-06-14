8dcd7724e642a0c813e13edfe291068f37ecfbd6
ambpro <aminboudeffa@gmail.com>
2020-06-14 18:19:25 +0200

    Add log4j logging

--- src/main/java/io/ambpro/Launcher.java
+++ src/main/java/io/ambpro/Launcher.java
@@ -1,12 +1,29 @@
 package io.ambpro;
 
 import java.util.Arrays;
+import java.util.List;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
 
 import io.ambpro.partition.Partition;
 
+/**
+ * @author aminboudeffa
+ * @param <T>
+ *
+ */
 public class Launcher {
 
+	final static Logger logger = Logger.getLogger(Launcher.class);
+
 	public static void main(String[] args) {
-		System.out.println(Partition.partitionList(Arrays.asList(1,2,3,4,5), 2));
+		BasicConfigurator.configure();
+		
+		List<Integer> inputList = Arrays.asList(1,2,3,4,5);
+		int size = 2;
+		
+		logger.info("partition(" + inputList + "," + size + ") retourne:" + Partition.partitionList(inputList, size));
 	}
+	
 }
